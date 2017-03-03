ff04c0bec0691b57e6a106b471c3e799c55866fe
zhedian <cooldeer@126.com>
2017-03-03 19:05:56 +0800

    add button

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -61,8 +61,16 @@ class TopicsController < ApplicationController
     end
   end
 
+  def upvote
+    @topic = Topic.find(params[:id])
+    @topic.votes.create
+    redirect_to(topics_path)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
+
+
     def set_topic
       @topic = Topic.find(params[:id])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-    resources :topics
-    root "topics#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root 'topics#index'
+ resources :topics do
+   member do
+     post 'upvote'
+   end
+ end
 end
