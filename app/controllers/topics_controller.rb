f75e2ed923dec834deedb399222a612d399ccc98
zhedian <cooldeer@126.com>
2017-03-06 19:04:48 +0800

    加分题

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
   # GET /topics
   # GET /topics.json
   def index
-    @topics = Topic.all
+    @topics = Topic.all    
   end
 
   # GET /topics/1
@@ -64,6 +64,14 @@ class TopicsController < ApplicationController
   def upvote
     @topic = Topic.find(params[:id])
     @topic.votes.create
+    redirect_to(topics_path)
+  end
+
+  def downvote
+    @topic = Topic.find(params[:id])
+    if @topic.votes.count > 0
+       @topic.votes.first.destroy
+    end
     redirect_to(topics_path)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'about' => 'topics#about'
+
  root 'topics#index'
  resources :topics do
    member do
      post 'upvote'
+     post 'downvote'
    end
  end
 end
